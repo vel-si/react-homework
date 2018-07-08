74931de24530df3d99fdf2f1b2f59a33b7760e11
Alexey <gladunalexv@gmail.com>
2018-07-08 09:46:54 +0300

    day2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components';
+import Header from './components/Header'
+import Menu from './components/Menu'
+import Main from './components/Main'
 
+const url = process.env.PUBLIC_URL + '/img/bg.jpg'
+const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 49px;
+  background-color: #242424;
+  opacity: 0.8;
+`
+const MenuWrapper = styled.div`
+  height: 89px;
+  padding-top: 20px;
+`
+const MainWrapper = styled.main`
+  height: 600px;
+  padding-top: 130px;
+  background: url(${url}) no-repeat;
+  background-size: cover;
+`
 class App extends Component {
   render() {
     return (
-      <div className="predict">
-        <p>Россия <b><GetRandom min="0" max="5" /> : <GetRandom min="0" max="5" /></b> Словакия</p>
+      <div ClassName="App">
+        <HeaderWrapper>  
+          <Grid>
+            <Header />
+          </Grid>
+        </HeaderWrapper>
+        <MenuWrapper>
+          <Grid>
+            <Menu />
+          </Grid>
+        </MenuWrapper>
+        <MainWrapper>
+          <Grid>
+            <Main />
+          </Grid>
+        </MainWrapper>
       </div>
+      
     );
   }
 }
 
-function GetRandom(props) {
-  return Math.floor(Math.random() * (Number(props.max) - Number(props.min) + 1)) + Number(props.min);
- }
-  
 export default App;
